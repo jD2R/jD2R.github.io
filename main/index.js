0a4efdf12e4b6cbf6072fc02a242be80b51efad2
Dominic R <drocco2307@gmail.com>
2022-12-11 18:13:59 -0500

    Update index.js

--- main/index.js
+++ main/index.js
@@ -1,6 +1,1 @@
-console.log('Hello World');
-document.querySelector('p').innerHTML = 'This was changed with JS.'
-
-const subHead = document.createElement('h3');
-subHead.innerHTML = 'This was created with <b>JS</b>.';
-document.body.appendChild(subHead);
+console.log('Hello World!');
